5a553f7d5143f0697f216da2bc6b7e5a32ed5f91
ChadCarey <ChadCarey7@gmail.com>
2018-03-21 21:26:46 -0500

    Added get by name method to exercise type

--- Tables.py
+++ Tables.py
@@ -3,21 +3,43 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
 
-_dbEngine = sqlalchemy.create_engine("sqlite:///database", echo=True)
+_dbEngine = sqlalchemy.create_engine("sqlite:///database", echo=False)
 _TableBase = declarative_base()
-_Database = sessionmaker(bind=_dbEngine)()
+_Session = sessionmaker(bind=_dbEngine)()
 
 
 class ExerciseType(_TableBase):
     __tablename__ = "exercise_types"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
+
+    @staticmethod
+    def Count(name):
+        return _Session.query(ExerciseType).filter(ExerciseType.name == name.lower()).count()
 
     @staticmethod
     def Post(name):
-        exercise = ExerciseType(name=name)
-        _Database.add(exercise)
-        _Database.commit()
-        return exercise.id
+        exercise = ExerciseType.NullObject()
+        if ExerciseType.Count(name) == 0:
+            exercise = ExerciseType(name=name.lower())
+            _Session.add(exercise)
+            _Session.commit()
+        else:
+            exercise = ExerciseType.GetByName(name)
+        return exercise
+
+    @staticmethod
+    def GetByName(name):
+        try:
+            query = _Session.query(ExerciseType).filter(ExerciseType.name == name.lower())
+            ex = query.first()
+            return ex
+        except:
+            return ExerciseType.NullObject()
+
+    @staticmethod
+    def NullObject():
+        return ExerciseType()
+
 
 _TableBase.metadata.create_all(_dbEngine)
--- TablesTest.py
+++ TablesTest.py
@@ -1,21 +1,39 @@
 import unittest
-from Tables import ExerciseType, _Database
+from Tables import ExerciseType
 
 
 class TestExerciseTypesTable(unittest.TestCase):
-    def test_add(self):
-        ex = ExerciseType(name="cardio")
-        _Database.add(ex)
-        _Database.commit()
-        self.assertTrue(ex.id is not None)
 
-    def test_post_method(self):
-        ex_id = ExerciseType.Post(name="Strength")
-        self.assertIsNotNone(ex_id)
+    def test_ExerciseType_post_method_adds_item_to_database(self):
+        ex = ExerciseType.Post(name="Strength")
+        self.assertIsNotNone(ex.id)
 
-    # def test_remove(self):
-    #     self.assertTrue(False)
+    def test_posting_ExerciseType_twice_returns_the_same_object(self):
+        ex = ExerciseType.Post(name="Strength")
+        ex2 = ExerciseType.Post(name="Strength")
+        self.assertEqual(ex.id, ex2.id)
 
+    def test_ExerciseTypes_are_unique(self):
+        name = "Strength"
+        ExerciseType.Post(name=name)
+        ExerciseType.Post(name=name)
+        count = ExerciseType.Count(name=name)
+        self.assertEqual(count, 1)
+
+    def test_ExerciseType_NullObject_method_returns_uninitialized_object(self):
+        ex = ExerciseType.NullObject()
+        self.assertIsNotNone(ex)
+        self.assertIsNone(ex.id)
+        self.assertIsNone(ex.name)
+
+    def test_ExerciseType_GetByName_method_returns_correct_object(self):
+        exName = "Plyometrics"
+        ex = ExerciseType.Post(exName)
+        ex2 = ExerciseType.GetByName(exName)
+        self.assertIsNotNone(ex.id)
+        self.assertEqual(ex.id, ex2.id)
 
 if __name__ == "__main__":
     unittest.main()
+    from Tables import _Database, _dbEngine
+    _Database.drop(_dbEngine)
