f3dde945e1b8b27bd4cd6300d4e33ba0950a0338
ChadCarey <ChadCarey7@gmail.com>
2018-03-24 20:23:18 -0500

    Removed print statements from APITest.py

--- APITest.py
+++ APITest.py
@@ -17,7 +17,6 @@ class TestAPI(unittest.TestCase):
         time.sleep(0.1)
 
     def test_post_exercise_type(self):
-        print "Starting test"
         r = requests.post('/'.join([TEST_URL, 'workout_type']))
         self.assertTrue(r.status_code == SUCCESS_STATUS)
 
